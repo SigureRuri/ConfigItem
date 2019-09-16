44299ef4287eb10d603fe4e093717d2fa83ad9ba
SigureRuri <50079507+SigureRuri@users.noreply.github.com>
2019-09-16 17:35:10 +0900

    test

---
+++ src/main/kotlin/com/github/sigureruri/configitem/ConfigItem.kt
@@ -0,0 +1,15 @@
+package com.github.sigureruri.configitem
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ConfigItem : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
